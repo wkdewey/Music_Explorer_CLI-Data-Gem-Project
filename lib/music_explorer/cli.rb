396bdbc09c0c7c48aeb21f854ea3b39d73833e8c
Your Name <wkdewey@gmail.com>
2020-06-05 09:18:29 -0400

    Create stub classes in CLI

--- lib/music_explorer/cli.rb
+++ lib/music_explorer/cli.rb
@@ -1,2 +1,31 @@
 class MusicExplorer::CLI
+
+  def call
+  end
+
+  def program_options
+  end
+
+  def search_artists
+  end
+
+  def display_artist
+  end
+
+  def artist_options
+  end
+
+  def display_top_tracks
+  end
+
+  def display_albums
+  end
+
+  def display_related_artists
+  end
+
+  def exit
+  end
+
+
 end
