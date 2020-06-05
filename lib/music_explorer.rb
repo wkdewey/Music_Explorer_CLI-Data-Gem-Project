011218f3905b75123507aa85391f9b9bfdc23f2c
Your Name <wkdewey@gmail.com>
2020-06-04 20:00:19 -0400

    Create music_explorer.rb, base file with requirements

---
+++ lib/music_explorer.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'httparty'
+
+
+require_relative "music_explorer/cli.rb"
+require_relative "music_explorer/artist.rb"
+require_relative "music_explorer/api.rb"
+
+module MusicExplorer
+end
